edf698c040fcce4b0a72e7da56bc2d9b5c8aea78
liorko87 <lior121987@gmail.com>
2019-12-19 12:35:25 +0200

    change executable name

--- MATRIX/run.sh
+++ MATRIX/run.sh
@@ -1,2 +1,2 @@
-./Comarison -partyID ${1} -partiesNumber ${2} -fieldType ${3} -partiesFile ${4} -delta ${5} -lambda ${6} -internalIterationsNumber ${7} &
+./Comparison -partyID ${1} -partiesNumber ${2} -fieldType ${3} -partiesFile ${4} -delta ${5} -lambda ${6} -internalIterationsNumber ${7} &
 
