154735b5d96bcd30a596c8736ae2e8e0dc35caad
liorko87 <lior121987@gmail.com>
2019-12-19 12:23:44 +0200

    Change paths for scripts

--- MATRIX/run.sh
+++ MATRIX/run.sh
@@ -1,2 +1,2 @@
-cd .. && ./Comarison -partyID ${1} -partiesNumber ${2} -fieldType ${3} -partiesFile ${4} -delta ${5} -lambda ${6} -internalIterationsNumber ${7} &
+./Comarison -partyID ${1} -partiesNumber ${2} -fieldType ${3} -partiesFile ${4} -delta ${5} -lambda ${6} -internalIterationsNumber ${7} &
 
