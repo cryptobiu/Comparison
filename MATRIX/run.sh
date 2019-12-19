ffe04abf1cff0eb662cb6058062ffe29bcd8c6cc
liorko87 <lior121987@gmail.com>
2019-12-19 13:56:45 +0200

    protcess does not work in the background

--- MATRIX/run.sh
+++ MATRIX/run.sh
@@ -1,2 +1,2 @@
-./Comparison -partyID ${1} -partiesNumber ${2} -fieldType ${3} -partiesFile ${4} -delta ${5} -lambda ${6} -numThreads ${7} -internalIterationsNumber ${8} &
+./Comparison -partyID ${1} -partiesNumber ${2} -fieldType ${3} -partiesFile ${4} -delta ${5} -lambda ${6} -numThreads ${7} -internalIterationsNumber ${8} 
 
